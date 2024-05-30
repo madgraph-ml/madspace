78f3ab85fcefb34aaa07d76b7be90a1a1be725f3
Theo Heimel <info@theovention.de>
2024-05-30 12:18:41 -0600

    added tests/examples for learnable mappings

---
+++ madspace/spline_mapping.py
@@ -0,0 +1,177 @@
+from math import prod
+from copy import copy
+
+import torch
+from torch import Tensor
+import torch.nn as nn
+
+from .base import PhaseSpaceMapping, TensorList, ShapeList
+from .functional.splines import unconstrained_rational_quadratic_spline
+
+class SplineMapping(PhaseSpaceMapping):
+    def __init__(
+        self,
+        mapping: PhaseSpaceMapping,
+        flow_dims_c: ShapeList = [],
+        extra_params_c: int = 0,
+        correlations: str = "none",
+        permutation: list[int] | None = None,
+        layers: int = 3,
+        hidden_dim: int = 32,
+        bins: int = 10,
+        activation: type[nn.Module] = nn.ReLU,
+        periodic: list[int] = [],
+        min_bin_width: float = 1e-3,
+        min_bin_height: float = 1e-3,
+        min_derivative: float = 1e-3,
+    ):
+        mapping_dims_c = [] if mapping.dims_c is None else mapping.dims_c
+        dims_in = mapping.dims_in
+        dims_out = mapping.dims_out
+        dims_c = mapping_dims_c + flow_dims_c
+        super().__init__(dims_in, dims_out, dims_c)
+
+        (self.r_dim,) = dims_in[0]
+        self.flat_c_dim = sum(prod(dim) for dim in flow_dims_c) + extra_params_c
+
+        self.mapping_dims_c = mapping_dims_c
+        self.flow_dims_c = flow_dims_c
+        self.mapping = mapping
+        self.correlations = correlations
+        self.bins = bins
+        self.layers = layers
+        self.hidden_dim = hidden_dim
+        self.activation = activation
+        self.min_bin_width = min_bin_width
+        self.min_bin_height = min_bin_height
+        self.min_derivative = min_derivative
+
+        if permutation is None:
+            permutation = list(range(self.r_dim))
+        self.permutation = permutation
+        self.inverse_permutation = [permutation.index(i) for i in range(self.r_dim)]
+        self.periodic = [permutation[i] in periodic for i in range(self.r_dim)]
+
+        self.submodules = nn.ModuleList()
+        self.subparams = nn.ParameterList()
+        if correlations == "none":
+            self.subnets = [self._build_subnet(self.flat_c_dim) for _ in range(self.r_dim)]
+        elif correlations == "autoregressive":
+            self.subnets = [self._build_subnet(self.flat_c_dim + i) for i in range(self.r_dim)]
+        elif correlations == "all":
+            self.subnets = [
+                self._build_subnet(self.flat_c_dim + self.r_dim - 1) for _ in range(self.r_dim)
+            ]
+        else:
+            raise ValueError("Unknown correlation mode")
+
+        if extra_params_c == 0:
+            self.extra_params_c = None
+        else:
+            self.extra_params_c = nn.Parameter(torch.zeros(extra_params_c))
+
+    def shared_mapping(self, mapping: PhaseSpaceMapping):
+        shared = copy(self)
+        shared._parameters = copy(self._parameters)
+        shared._modules = copy(self._modules)
+        shared.mapping = mapping
+        if shared.extra_params_c is not None:
+            old_params_c = self.extra_params_c
+            shared.extra_params_c = nn.Parameter(torch.zeros_like(self.extra_params_c))
+        return shared
+
+    def _build_subnet(
+        self,
+        dims_in: int,
+    ) -> nn.Module | nn.Parameter:
+        dims_out = 3 * self.bins + 1
+        if dims_in == 0:
+            param = nn.Parameter(torch.zeros(dims_out))
+            self.subparams.append(param)
+            return param
+
+        modules = []
+        layer_dim = dims_in
+        for i in range(self.layers - 1):
+            modules.append(nn.Linear(layer_dim, self.hidden_dim))
+            modules.append(self.activation())
+            layer_dim = self.hidden_dim
+        last_layer = nn.Linear(layer_dim, dims_out)
+        nn.init.zeros_(last_layer.weight)
+        nn.init.zeros_(last_layer.bias)
+        modules.append(last_layer)
+
+        module = nn.Sequential(*modules)
+        self.submodules.append(module)
+        return module
+
+    def map(self, inputs: TensorList, condition=None):
+        r, rest = inputs[0], inputs[1:]
+        c_mapping, c_flow = self._split_condition(condition, r.shape[0])
+        x, jac_flow = self._map_flow(r, c_flow, inverse=False)
+        out, jac_mapping = self.mapping.map([x, *rest], c_mapping)
+        return out, jac_flow * jac_mapping
+
+    def map_inverse(self, inputs: TensorList, condition=None):
+        (x, *rest), jac_mapping = self.mapping.map_inverse(inputs, c_mapping)
+        c_mapping, c_flow = self._split_condition(condition, x.shape[0])
+        r, jac_flow = self._map_flow(x, c_flow, inverse=True)
+        return (r, *rest), jac_flow * jac_mapping
+
+    def _split_condition(self, condition: TensorList | None, batch_dim: int):
+        if condition is None:
+            condition = []
+        if self.extra_params_c is not None:
+            condition = [*condition, self.extra_params_c[None,:].expand(batch_dim, -1)]
+        n_mappings_dims_c = len(self.mapping_dims_c)
+        c_mapping = condition[:n_mappings_dims_c]
+        if n_mappings_dims_c < len(condition):
+            c_flow = torch.cat(
+                [c.reshape(batch_dim, -1) for c in condition[n_mappings_dims_c:]], dim=1
+            )
+        else:
+            c_flow = None
+        return c_mapping, c_flow
+
+    def _map_flow(self, x: Tensor, c: Tensor | None, inverse: bool) -> tuple[Tensor, Tensor]:
+        if inverse:
+            x = x[:, self.inverse_permutation]
+        else:
+            x = x[:, self.permutation]
+        if c is None:
+            c = x[:, :0]
+        log_jac_all = 0.
+        subnet_iter = enumerate(self.subnets)
+        for i, subnet in (reversed(list(subnet_iter)) if inverse else subnet_iter):
+            if isinstance(subnet, nn.Parameter):
+                theta = subnet[None, :].expand(x.shape[0], -1)
+            else:
+                if self.correlations == "none":
+                    c_all = c
+                elif self.correlations == "autoregressive":
+                    c_all = torch.cat((x[:, :i], c), dim=1)
+                elif self.correlations == "all":
+                    c_all = torch.cat((x[:, :i], x[:, i+1:], c), dim=1)
+                theta = subnet(c_all)
+
+            x[:, i:i+1], log_jac = unconstrained_rational_quadratic_spline(
+                inputs=x[:, i:i+1],
+                theta=theta[:, None],
+                rev=inverse,
+                num_bins=self.bins,
+                left=0.,
+                right=1.,
+                bottom=0.,
+                top=1.,
+                min_bin_width=self.min_bin_width,
+                min_bin_height=self.min_bin_height,
+                min_derivative=self.min_derivative,
+                periodic=self.periodic[i],
+            )
+            log_jac_all += log_jac
+
+        if inverse:
+            x = x[:, self.permutation]
+        else:
+            x = x[:, self.inverse_permutation]
+        return x, log_jac_all.exp()
---
+++ tests/test_learnable.py
@@ -0,0 +1,164 @@
+from icecream import ic
+import torch
+import torch.nn as nn
+
+from madspace.spline_mapping import SplineMapping
+from madspace.twoparticle import TwoParticleCOM, tInvariantTwoParticleCOM
+from madspace.invariants import BreitWignerInvariantBlock, UniformInvariantBlock
+from madspace.functional.ps_utils import build_p_in
+
+torch.set_default_dtype(torch.float64)
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+print("------------------------------------------------------------")
+print("TEST 1: Learn Breit-Wigner with fixed min/max")
+print("------------------------------------------------------------")
+
+uni_inv = UniformInvariantBlock()
+bw_inv = BreitWignerInvariantBlock(mass=90., width=4.)
+smap = SplineMapping(mapping=uni_inv, flow_dims_c=[])
+print("Trainable parameters:", count_parameters(smap))
+
+n = 1024
+
+opt = torch.optim.Adam(smap.parameters(), lr=1e-2)
+
+for i in range(1000):
+    opt.zero_grad()
+    r = torch.rand((n, 1))
+    s_min = torch.full_like(r, 1.**2)
+    s_max = torch.full_like(r, 1000.**2)
+    (s,), jac_fw = smap.map([r], [s_min, s_max])
+    (r_out,), jac_inv = bw_inv.map_inverse([s], [s_min, s_max])
+    loss = - torch.log(jac_fw * jac_inv).mean()
+    loss.backward()
+    opt.step()
+    if (i+1) % 50 == 0:
+        print(i + 1, loss.item())
+
+print()
+print("median |Δr|", (r - r_out).abs().median().item())
+print("median |Δlog jac|", (jac_fw.log() + jac_inv.log()).abs().median().item())
+
+print()
+print("------------------------------------------------------------")
+print("TEST 2: Learn Breit-Wigner conditioned on random min/max")
+print("------------------------------------------------------------")
+
+uni_inv = UniformInvariantBlock()
+bw_inv = BreitWignerInvariantBlock(mass=90., width=4.)
+smap = SplineMapping(mapping=uni_inv, flow_dims_c=[(2,1)])
+print("Trainable parameters:", count_parameters(smap))
+
+n = 1024
+
+opt = torch.optim.Adam(smap.parameters(), lr=1e-2)
+
+for i in range(1000):
+    opt.zero_grad()
+    r = torch.rand((n, 1))
+    r_min = torch.rand((n, 1))
+    r_max = torch.rand((n, 1))
+    s_min = (r_min * 80 + 1) ** 2
+    s_max = (r_max * 1000 + 100) ** 2
+    (s,), jac_fw = smap.map([r], [s_min, s_max, r_min, r_max])
+    (r_out,), jac_inv = bw_inv.map_inverse([s], [s_min, s_max])
+    loss = - torch.log(jac_fw * jac_inv).mean()
+    loss.backward()
+    opt.step()
+    if (i+1) % 50 == 0:
+        print(i + 1, loss.item())
+
+print()
+print("median |Δr|", (r - r_out).abs().median().item())
+print("median |Δlog jac|", (jac_fw.log() + jac_inv.log()).abs().median().item())
+
+print()
+print("------------------------------------------------------------")
+print("TEST 3: Learn two Breit-Wigners with shared network")
+print("------------------------------------------------------------")
+
+uni1_inv = UniformInvariantBlock()
+bw1_inv = BreitWignerInvariantBlock(mass=85., width=4.)
+smap1 = SplineMapping(mapping=uni1_inv, flow_dims_c=[(2,1)], extra_params_c=1)
+
+uni2_inv = UniformInvariantBlock()
+bw2_inv = BreitWignerInvariantBlock(mass=95., width=4.)
+smap2 = smap1.shared_mapping(uni2_inv)
+
+print(smap1.extra_params_c is smap2.extra_params_c)
+
+combined = nn.ModuleList([smap1, smap2])
+print("Trainable parameters:", count_parameters(combined))
+
+n = 1024
+
+opt = torch.optim.Adam(combined.parameters(), lr=1e-2)
+
+for i in range(1000):
+    opt.zero_grad()
+    r = torch.rand((2*n, 1))
+    r_min = torch.rand((2*n, 1))
+    r_max = torch.rand((2*n, 1))
+    s_min = (r_min * 80 + 1) ** 2
+    s_max = (r_max * 1000 + 100) ** 2
+
+    (s,), jac_fw1 = smap1.map([r[:n]], [s_min[:n], s_max[:n], r_min[:n], r_max[:n]])
+    (r_out1,), jac_inv1 = bw1_inv.map_inverse([s], [s_min[:n], s_max[:n]])
+
+    (s,), jac_fw2 = smap2.map([r[n:]], [s_min[n:], s_max[n:], r_min[n:], r_max[n:]])
+    (r_out2,), jac_inv2 = bw2_inv.map_inverse([s], [s_min[n:], s_max[n:]])
+
+    r_out = torch.cat((r_out1, r_out2), dim=0)
+    jac_fw = torch.cat((jac_fw1, jac_fw2), dim=0)
+    jac_inv = torch.cat((jac_inv1, jac_inv2), dim=0)
+    loss = - torch.log(jac_fw * jac_inv).mean()
+    loss.backward()
+    opt.step()
+    if (i+1) % 50 == 0:
+        print(i + 1, loss.item())
+
+print()
+print("median |Δr|", (r - r_out).abs().median().item())
+print("median |Δlog jac|", (jac_fw.log() + jac_inv.log()).abs().median().item())
+print("extra conditions", smap1.extra_params_c.item(), smap2.extra_params_c.item())
+
+print()
+print("------------------------------------------------------------")
+print("TEST 4: Learn 2D distribution")
+print("------------------------------------------------------------")
+
+iso = TwoParticleCOM()
+aniso = tInvariantTwoParticleCOM()
+smap = SplineMapping(
+    mapping=iso,
+    flow_dims_c=[],
+    correlations="all",
+    periodic=[0],
+    permutation=[0,1],
+)
+print("Trainable parameters:", count_parameters(smap))
+
+n = 1024
+opt = torch.optim.Adam(smap.parameters(), lr=1e-2)
+
+sqrt_s = torch.full((n,), 90.)
+s = sqrt_s.square()
+m_out = torch.zeros((n, 2))
+p_in = build_p_in(sqrt_s)
+
+for i in range(1000):
+    opt.zero_grad()
+    r = torch.rand((n, 2))
+    (p_decay,), jac_fw = smap.map([r, s, m_out])
+    (r_out, _), jac_inv = aniso.map_inverse([p_decay], [p_in])
+    loss = - torch.log(jac_fw * jac_inv).mean()
+    loss.backward()
+    opt.step()
+    if (i+1) % 50 == 0:
+        print(i + 1, loss.item())
+
+print()
+print("median |Δlog jac|", (jac_fw.log() + jac_inv.log()).abs().median().item())
