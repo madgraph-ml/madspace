Update wrong signs in t-channel

madspace/chili.py
@@ -9,7 +9,7 @@ from math import pi
 from torch import Tensor, sqrt, log, sinh, cos, sin, exp
 
 from .base import PhaseSpaceMapping, TensorList
-from .functional.kinematics import rapidity, pT2, lsquare, phi
+from .functional.kinematics import rapidity, pT2, lsquare, phi, mass
 
 
 class tChiliBlock(PhaseSpaceMapping):
@@ -176,7 +176,7 @@ class tChiliBlock(PhaseSpaceMapping):
         p_out = p_ext[:, 2:]
         s = lsquare(p_in.sum(dim=1))
         e_cm = sqrt(s)
-        m_out = sqrt(lsquare(p_out))
+        m_out = mass(p_out)
 
         # Get relevant psum and energies etc
         psum_nm1 = p_out[:, :-1]  # sum over final states except last particle

madspace/functional/kinematics.py
@@ -5,7 +5,6 @@ from torch import Tensor, cos, sin, cosh, sinh, sqrt, log, atan2
 from math import pi
 
 
-MINKOWSKI = torch.diag(torch.tensor([1.0, -1.0, -1.0, -1.0]))
 DTYE = torch.get_default_dtype()
 EPS = 1e-12 if DTYE == torch.float64 else 1e-6
 
@@ -352,12 +351,9 @@ def lsquare(a: Tensor) -> Tensor:
     Returns:
         Tensor: Lorentzscalar with shape=(b,...)
     """
-    #s = torch.einsum("...d,dd,...d->...", a, MINKOWSKI, a)
     a2 = a.square()
     s = a2[..., 0] - a2[..., 1] - a2[..., 2] - a2[..., 3]
-    #return s
-    #return torch.clamp_min_(s, EPS)
-    return torch.where(s.abs() < EPS, 0., s)
+    return s
 
 
 def ldot(a: Tensor, b: Tensor) -> Tensor:
@@ -373,7 +369,6 @@ def ldot(a: Tensor, b: Tensor) -> Tensor:
     """
     ab = a * b
     return ab[..., 0] - ab[..., 1] - ab[..., 2] - ab[..., 3]
-    #return torch.einsum("...d,dd,...d->...", a, MINKOWSKI, b)
 
 
 def esquare(a: Tensor) -> Tensor:
@@ -386,7 +381,8 @@ def esquare(a: Tensor) -> Tensor:
     Returns:
         Tensor: Square with shape=(b,...)
     """
-    return torch.einsum("...d,...d->...", a, a)
+    a2 = a.square()
+    return a2[..., 0] + a2[..., 1] + a2[..., 2] + a2[..., 3]
 
 
 def edot(a: Tensor, b: Tensor) -> Tensor:
@@ -400,7 +396,8 @@ def edot(a: Tensor, b: Tensor) -> Tensor:
     Returns:
         Tensor: Lorentzscalar with shape=(b,...)
     """
-    return torch.einsum("...d,...d->...", a, b)
+    ab = a * b
+    return ab[..., 0] + ab[..., 1] + ab[..., 2] + ab[..., 3]
 
 
 def boost(k: Tensor, p_boost: Tensor, inverse: bool = False) -> Tensor:

madspace/rambo.py
@@ -8,7 +8,7 @@ from .rootfinder.roots import get_u_parameter, get_xi_parameter
 
 from .base import PhaseSpaceMapping, TensorList
 from .luminosity import Luminosity, FlatLuminosity, ResonantLuminosity
-from .functional.kinematics import boost, boost_beam, mass, esquare, lsquare
+from .functional.kinematics import boost, boost_beam, mass, esquare, sqrt_shat
 from .functional.ps_utils import (
     build_p_in,
     pin_to_x1x2,
@@ -87,7 +87,7 @@ class Rambo(PhaseSpaceMapping):
         Q = q.sum(dim=1, keepdim=True)  # has shape (b,1,4)
 
         # Get scaling factor and match dimensions
-        M = sqrt(lsquare(Q))  # has shape (b,1)
+        M = mass(Q)  # has shape (b,1)
         x = e_cm / M  # has shape (b,1)
 
         # Boost and refactor
@@ -320,7 +320,7 @@ class RamboOnDiet(PhaseSpaceMapping):
         # Get input momenta
         p_ext = inputs[0]
         k = p_ext[:, 2:]
-        e_cm = sqrt(lsquare(k.sum(dim=1)))
+        e_cm = (k.sum(dim=1))
         w0 = self._massles_weight(e_cm)
 
         # Make momenta massless before going back to random numbers
@@ -345,7 +345,7 @@ class RamboOnDiet(PhaseSpaceMapping):
         # Get random numbers associated to the intermediate masses
         # have shapees (b, n-1)
         P = torch.cumsum(p.flip(1), dim=1)[:, 1:]  # has shape (b, n-1)
-        M = sqrt(lsquare(P))
+        M = mass(P)
         # have shapes (b, n-2)
         um = (M[:, :-1] / M[:, 1:]).flip(1)
         iarray = torch.arange(2, self.nparticles, device=p.device)[None, :]
@@ -823,7 +823,7 @@ class tRamboBlock(PhaseSpaceMapping):
         del condition
         # Get input momenta
         p_in, k = inputs
-        e_cm = sqrt(lsquare(p_in.sum(dim=1)))
+        e_cm = sqrt_shat(p_in)
         m_out = mass(k)  # has shape (b,n)
         w0 = self._massles_weight(e_cm)
 
@@ -841,7 +841,7 @@ class tRamboBlock(PhaseSpaceMapping):
         # Get random numbers associated to the intermediate masses
         # have shapees (b, n-1)
         P = torch.cumsum(p.flip(1), dim=1)[:, 1:]  # has shape (b, n-1)
-        M = sqrt(lsquare(P))
+        M = mass(P)
         # have shapes (b, n-2)
         um = (M[:, :-1] / M[:, 1:]).flip(1)
         iarray = torch.arange(2, self.nparticles, device=p.device)[None, :]
@@ -1001,7 +1001,7 @@ class tMahamboBlock(PhaseSpaceMapping):
         del condition
         p_in_lab = inputs[0]
         p_out_lab = inputs[1]
-        sqrt_s = sqrt(lsquare(p_in_lab.sum(dim=1)))
+        sqrt_s = sqrt_shat(p_in_lab)
         x1x2 = pin_to_x1x2(p_in_lab, sqrt_s)
 
         # Get rapidities
@@ -1151,7 +1151,7 @@ class tAutoregressiveMahamboBlock(PhaseSpaceMapping):
         del condition
         p_in_lab = inputs[0]
         p_out_lab = inputs[1]
-        sqrt_s = sqrt(lsquare(p_in_lab.sum(dim=1)))
+        sqrt_s = sqrt_shat(p_in_lab)
         x1x2 = pin_to_x1x2(p_in_lab, sqrt_s)
 
         # Get rapidities